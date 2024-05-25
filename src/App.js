e1aafcf3e48ab4d371568349110b8c2699e08e18
Piyush cn <piyush.gogoriya@capitalnow.in>
2024-05-25 18:58:02 +0530

    Tailwind css integration and project structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div className="h-screen bg-black">
+      <div className="h-[90%] flex"></div>
+    </div>
+  );
 };
 
 export default App;
